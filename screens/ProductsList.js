352c87d80f31343979d05f40e88da5d3d696daed
Shahid Shafique <shahidshafique@users.noreply.github.com>
2021-11-27 20:15:33 +0000

    View Product list

--- screens/ProductsList.js
+++ screens/ProductsList.js
@@ -1,12 +1,50 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, Text, View, FlatList} from 'react-native';
+import {ProductCard} from '../components/ProductCard';
+import {getProducts} from '../services/ProductsService';
 
-function ProductsList() {
+function ProductsList({navigation}) {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await getProducts();
+      setProducts(response);
+    }
+    fetchData();
+  }, []);
+
+  function renderProduct({item: product}) {
+    return (
+      <ProductCard
+        {...product}
+        onPress={() => {
+          navigation.navigate('ProductDetails', {
+            productId: product.id,
+          });
+        }}
+      />
+    );
+  }
+
   return (
-    <View>
-      <Text>Product List</Text>
-    </View>
+    <FlatList
+      style={styles.productsList}
+      contentContainerStyle={styles.productsListContainer}
+      keyExtractor={item => item.id.toString()}
+      data={products}
+      renderItem={renderProduct}
+    />
   );
 }
-
+const styles = StyleSheet.create({
+  productsList: {
+    backgroundColor: '#eeeeee',
+  },
+  productsListContainer: {
+    backgroundColor: '#eeeeee',
+    paddingVertical: 8,
+    marginHorizontal: 8,
+  },
+});
 export default ProductsList;
